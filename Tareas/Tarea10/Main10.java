568a69ab1f9576e390725723ec57085eb80f03ef
FabianCruz-0 <fabiancruz3773@gmail.com>
2020-05-19 23:32:59 -0500

    java 10 terminado

---
+++ Tareas/Tarea10/Main10.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Main10 {
+	public static void main(String[] args) {
+		String entrada;
+		int i, n;
+		double re = 0;
+		entrada = JOptionPane.showInputDialog("cantidad de serie a generar:");
+		n = Integer.parseInt(entrada);
+		for (i = 1; i <= n; i++) {
+			if (i % 2 == 0) {
+				re = (Math.pow(i, (i * 2 - 1)));
+			} else {
+
+				re = (Math.pow(i, (i * 2 - 1)));
+			}
+			JOptionPane.showMessageDialog(null, i + ".- " + re);
+		}
+	}
+}
